7ff6ea235e92fad72908bed7162c20fdc2831986
jameshuang623 <363753466@qq.com>
2021-09-27 00:34:16 +0800

    ref属性(修改目录结构)

---
+++ 03-使用Vue脚手架/vue-test/02_src_ref属性/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1 v-text="msg" ref="title"></h1>
+    <button @click="showDOM" ref="btn">点我输出上方的 DOM 元素</button>
+    <School ref="sch"></School>
+  </div>
+</template>
+
+<script lang="js">
+import School from "./components/School";
+export default {
+  name: "App",
+  components:{School},
+  data(){
+    return{
+      msg: '欢迎学习Vue！'
+    }
+  },
+  methods:{
+    showDOM(){
+      console.log(this.$refs.title)
+      console.log(this.$refs.btn)
+      console.log(this.$refs.sch)
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
