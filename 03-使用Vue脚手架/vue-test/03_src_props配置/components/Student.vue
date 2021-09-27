486e3034fe3a03556f2b41d25a4e275945f58e0c
jameshuang623 <363753466@qq.com>
2021-09-27 09:02:55 +0800

    props配置

---
+++ 03-使用Vue脚手架/vue-test/03_src_props配置/components/Student.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <h2>学生姓名：{{name}}</h2>
+    <h2>学生性别：{{sex}}</h2>
+    <h2>学生年龄：{{myAge+1}}</h2>
+    <button @click="updateAge">尝试修改收到的年龄</button>
+  </div>
+</template>
+
+<script lang="js">
+export default {
+  name: "Student",
+  data(){
+    return {
+      myAge: this.age // props的优先级更高
+    }
+  },
+  methods:{
+    updateAge(){
+      this.myAge++
+    }
+  },
+  // props:['name', 'sex', 'age'] // 简单接收
+
+
+  // 接收的同时对数据进行类型限制
+  // props:{
+  //   name: String,
+  //   age: Number,
+  //   sex: String
+  // }
+
+
+  // 接收的同时对数据进行类型限制+默认值的指定+必要性的限制
+  props:{
+    name:{
+      type: String,
+      required: true // name 是必要的
+    },
+    age:{
+      type: Number,
+      default: 99
+    },
+    sex:{
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
