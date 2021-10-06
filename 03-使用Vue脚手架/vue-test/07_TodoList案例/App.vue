db1abb84124d62a4f878ec62e93415ab7a59f79f
jameshuang623 <363753466@qq.com>
2021-10-06 16:34:23 +0800

    23_求和案例_纯vue版

---
+++ 03-使用Vue脚手架/vue-test/07_TodoList案例/App.vue
@@ -0,0 +1,16 @@
+<template>
+</template>
+
+<script lang="js">
+import MyHeader from "./components/MyHeader";
+import MyList from "./components/MyList";
+import MyFooter from "./components/MyFooter";
+export default {
+  name: "App",
+  components:{MyHeader,MyList,MyFooter},
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- 03-使用Vue脚手架/vue-test/07_TodoList案例/main.js
+++ 03-使用Vue脚手架/vue-test/07_TodoList案例/main.js
@@ -1,13 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Vuex from 'vuex'
 
 Vue.config.productionTip = false
 
-Vue.use(Vuex)
 
 new Vue({
     el: '#app',
-
     render: h => h(App)
 })
