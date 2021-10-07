89c6ba57ee5d17953f30ad168956ef0a85c8113c
jameshuang623 <363753466@qq.com>
2021-10-07 21:59:09 +0800

    完成 41_独享路由守卫

---
+++ 03-使用Vue脚手架/vue-test/41_独享路由守卫/pages/Message.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="m in messageList" :key="m.id">
+        <!-- 跳转路由并携带 params 参数，to 的字符串写法 -->
+        <!--        <router-link :to="`/home/message/detail/${m.id}/${m.title}`">{{ m.title }}</router-link>-->
+
+
+        <!--        跳转路由并携带 params 参数，to 的对象写法-->
+        <router-link :to="{
+                  name: 'xiangqing', // 注意，当使用 params 参数时，这个只能用 name，用 path 会错误！！
+                  query: {
+                    id: m.id,
+                    title: m.title
+                  }
+                }">
+          {{ m.title }}
+        </router-link>
+        <button @click="pushShow(m)">push 查看</button>
+        <button @click="replaceShow(m)">replace 查看</button>
+      </li>
+    </ul>
+    <hr>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script lang="js">
+export default {
+  name: "Message",
+  data() {
+    return {
+      messageList: [
+        {id: '001', title: '消息001'},
+        {id: '002', title: '消息002'},
+        {id: '003', title: '消息003'},
+      ]
+    }
+  },
+  methods:{
+    pushShow(m){
+      this.$router.push({
+        name: 'xiangqing',
+        query: {
+          id: m.id,
+          title: m.title
+        }
+      })
+    },
+    replaceShow(m){
+      this.$router.replace({
+        name: 'xiangqing',
+        query: {
+          id: m.id,
+          title: m.title
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
