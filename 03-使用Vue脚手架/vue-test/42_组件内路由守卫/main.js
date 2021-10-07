c24f80f2d4dc74913016adf46d9f417b6de2dd67
jameshuang623 <363753466@qq.com>
2021-10-07 22:42:37 +0800

    完成 42_组件内路由守卫

---
+++ 03-使用Vue脚手架/vue-test/42_组件内路由守卫/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueRouter from "vue-router";
+// 必须叫router
+import router from './router'
+
+Vue.config.productionTip = false
+
+Vue.use(VueRouter)
+
+
+new Vue({
+    el: '#app',
+    render: h => h(App),
+    router // 必须叫 router
+})
---
+++ 03-使用Vue脚手架/vue-test/42_组件内路由守卫/pages/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>我是Home的内容</h2>
+    <router-link to="/home/news">News</router-link>
+    <router-link to="/home/message">Message</router-link>
+    <keep-alive :include="['News', 'Message']"> <!-- include 里指的是组件名（name）-->
+      <router-view></router-view>
+    </keep-alive>
+  </div>
+</template>
+
+<script lang="js">
+export default {
+  name: "Home"
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
