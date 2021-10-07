db9e717b6cfbc68086a966ea66b3e6e4e804c328
jameshuang623 <363753466@qq.com>
2021-10-07 13:54:02 +0800

    完成 33_命名路由

---
+++ 03-使用Vue脚手架/vue-test/33_命名路由/components/Banner.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>vue-router demo</h1>
+  </div>
+</template>
+
+<script lang="js">
+export default {
+  name: "Banner"
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ 03-使用Vue脚手架/vue-test/33_命名路由/pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>我是Home的内容</h2>
+    <router-link to="/home/news">News</router-link>
+    <router-link to="/home/message">Message</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script lang="js">
+export default {
+  name: "Home"
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
