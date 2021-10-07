1bec059267896028cc4d66d5163180969f388abb
jameshuang623 <363753466@qq.com>
2021-10-07 10:41:37 +0800

    完成 31_嵌套路由

---
+++ 03-使用Vue脚手架/vue-test/31_嵌套路由/pages/Message.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <ul>
+      <li>message01</li>
+      <li>message02</li>
+      <li>message03</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="js">
+export default {
+  name: "Message"
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- 03-使用Vue脚手架/vue-test/src/pages/About.vue
+++ 03-使用Vue脚手架/vue-test/src/pages/About.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <h2>我是Home的内容</h2>
+    <h2>我是About的内容</h2>
   </div>
 </template>
 
 <script lang="js">
 export default {
-  name: "Home"
+  name: "About"
 }
 </script>
 
