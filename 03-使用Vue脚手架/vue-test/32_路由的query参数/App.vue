409951ebb223fcabd9b6472cc8d0f93a43d80d19
jameshuang623 <363753466@qq.com>
2021-10-07 13:46:26 +0800

    完成 32_路由的query参数

---
+++ 03-使用Vue脚手架/vue-test/32_路由的query参数/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Banner></Banner>
+    <router-link to="/about">About</router-link>
+    <router-link to="/home">Home</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script lang="js">
+import Banner from "./components/Banner";
+export default {
+  name: "App",
+  components:{Banner}
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
